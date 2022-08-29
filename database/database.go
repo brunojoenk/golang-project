626187caa3c3d2310b99535134f61b247e5d1338
Bruno <bjoenk@gmail.com>
2022-08-28 21:40:01 -0300

    fix:deleted log

--- database/database.go
+++ database/database.go
@@ -19,8 +19,6 @@ func NewPsqlDB(c *config.Config) (*gorm.DB, error) {
 		c.Postgres.PostgresqlDbname,
 		c.Postgres.PostgresqlPort)
 
-	fmt.Println("Datasourcename: " + dataSourceName)
-
 	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
