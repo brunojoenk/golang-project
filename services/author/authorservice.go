367bad8af4693d721de0ba3700ca42b118520aaf
Bruno <bjoenk@gmail.com>
2022-08-30 14:27:35 -0300

    feat: turn concurrence func of authors import from csv file

--- services/author/authorservice.go
+++ services/author/authorservice.go
@@ -5,6 +5,7 @@ import (
 	"github/brunojoenk/golang-test/models"
 	authorrepo "github/brunojoenk/golang-test/repository/author"
 	"os"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -50,8 +51,8 @@ func (a *AuthorService) GetAllAuthors(filter models.GetAuthorsFilter) (*models.A
 	return authorResponseMetada, nil
 }
 
+// Import all author using concurrence
 func (a *AuthorService) ImportAuthorsFromCSVFile(file string) ([]string, error) {
-
 	f, err := os.Open(file)
 
 	if err != nil {
@@ -61,44 +62,84 @@ func (a *AuthorService) ImportAuthorsFromCSVFile(file string) ([]string, error) 
 
 	defer f.Close()
 
-	r := csv.NewReader(f)
-	r.Comma = ';'
+	fcsv := csv.NewReader(f)
+	fcsv.Comma = ';'
+	authors := make([]*models.Author, 0)
+	numWorkers := 20
+	jobs := make(chan []*models.Author, numWorkers)
+	res := make(chan []*models.Author)
 
-	records, err := r.ReadAll()
+	batchSize := 1000
 
-	if err != nil {
-		log.Error("Error on readall from file: ", err.Error())
-		return nil, err
+	var wg sync.WaitGroup
+	worker := func(jobs <-chan []*models.Author, results chan<- []*models.Author) error {
+		for {
+			select {
+			case job, ok := <-jobs: // you must check for readable state of the channel.
+				if !ok {
+					return nil
+				}
+				err := a.createAuthorInBatchRepo(job, len(job))
+				if err != nil {
+					log.Error("Error on create author in batch repository: ", err.Error())
+					return err
+				}
+				results <- job
+			}
+		}
+	}
+
+	var errOnBatch error
+	// init workers
+	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
+		go func() {
+			// this line will exec when chan `res` processed
+			defer wg.Done()
+			errOnBatch = worker(jobs, res)
+		}()
+	}
+
+	go func() {
+		mapper := make(map[string]bool, 0)
+		rStr, err := fcsv.ReadAll()
+		if err != nil {
+			log.Error("Error on read all csv: ", err.Error())
+			return
+		}
+		for _, record := range rStr {
+			count := 0
+			batch := make([]*models.Author, 0)
+			for i, name := range record {
+				if !mapper[name] {
+					mapper[name] = true
+					count++
+					batch = append(batch, &models.Author{Name: name})
+				}
+				if count == batchSize || i == (len(record)-1) {
+					jobs <- batch
+					batch = make([]*models.Author, 0)
+					count = 0
+				}
+			}
+		}
+
+		close(jobs) // close jobs to signal workers that no more job are incoming.
+	}()
+
+	go func() {
+		wg.Wait()
+		close(res) // when you close(res) it breaks the below loop.
+	}()
+
+	for r := range res {
+		authors = append(authors, r...)
 	}
 
 	names := make([]string, 0)
-	mapper := make(map[string]bool, 0)
-
-	batchSize := 2000
-
-	for _, record := range records {
-		count := 0
-		var batch []*models.Author
-		for i, name := range record {
-			if !mapper[name] {
-				mapper[name] = true
-				count++
-				batch = append(batch, &models.Author{Name: name})
-				names = append(names, name)
-			}
-			if count == batchSize || i == (len(record)-1) {
-				err := a.createAuthorInBatchRepo(batch, count)
-				if err != nil {
-					log.Error("Error on create author in batch repository: ", err.Error())
-					return nil, err
-				}
-				batch = make([]*models.Author, 0)
-				count = 0
-			}
-
-		}
-
+	for _, a := range authors {
+		names = append(names, a.Name)
 	}
 
-	return names, err
+	return names, errOnBatch
 }
