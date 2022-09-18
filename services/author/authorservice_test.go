923aec37ebd00b201938910ce25fd5d9511bef39
Bruno Joenk <bjoenk@gmail.com>
2022-09-18 14:24:50 -0300

    feat: changed tests to use testify/mock and interfaces

--- services/author/authorservice_test.go
+++ services/author/authorservice_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"errors"
 	"github/brunojoenk/golang-test/models/dtos"
 	"github/brunojoenk/golang-test/models/entities"
 	"testing"
@@ -43,4 +44,51 @@ func TestGetAllAuthors(t *testing.T) {
 	require.Nil(t, deep.Equal([]dtos.AuthorResponse{{Id: 5, Name: "Joenk"}}, resp.Authors))
 	require.Equal(t, resp.Pagination.Limit, 10)
 	require.Equal(t, resp.Pagination.Page, 1)
+}
+
+func TestGetAllAuthorsError(t *testing.T) {
+
+	authorDbMock := new(authorDbMock)
+
+	filter := dtos.GetAuthorsFilter{Pagination: dtos.Pagination{Page: 1, Limit: 10}}
+	authorDbMock.On("GetAllAuthors", filter).Return([]entities.Author{}, errors.New("Error on test"))
+
+	authorServiceTest := AuthorService{authorDb: authorDbMock}
+
+	_, err := authorServiceTest.GetAllAuthors(filter)
+	require.Error(t, err)
+}
+
+func TestImportAuthorsFromCSVFile(t *testing.T) {
+	authorDbMock := new(authorDbMock)
+
+	authorDbMock.On("CreateAuthorInBatch", mock.Anything, 6).Return(nil)
+
+	authorServiceTest := AuthorService{authorDb: authorDbMock}
+
+	resp, err := authorServiceTest.ImportAuthorsFromCSVFile("../../data/authorsreduced.csv")
+	require.NoError(t, err)
+	require.Equal(t, 6, resp)
+}
+
+func TestImportAuthorsFromCSVFileErronOnCreateAuthorInBatch(t *testing.T) {
+	authorDbMock := new(authorDbMock)
+
+	authorDbMock.On("CreateAuthorInBatch", mock.Anything, 6).Return(errors.New("error occurred"))
+
+	authorServiceTest := AuthorService{authorDb: authorDbMock}
+
+	resp, err := authorServiceTest.ImportAuthorsFromCSVFile("../../data/authorsreduced.csv")
+
+	require.Error(t, err)
+	require.Equal(t, 0, resp)
+}
+
+func TestImportAuthorsFromCSVFileError(t *testing.T) {
+	authorDbMock := new(authorDbMock)
+
+	authorServiceTest := AuthorService{authorDb: authorDbMock}
+
+	_, err := authorServiceTest.ImportAuthorsFromCSVFile("anyfile")
+	require.Error(t, err)
 }
